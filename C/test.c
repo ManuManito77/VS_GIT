88372227332622c17e70c013348c61f3076b4bcd
ManuManito77 <86896933+ManuManito77@users.noreply.github.com>
2021-09-15 01:05:52 -0300

    Test de C

--- C/test.c
+++ C/test.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 
 int main(){
-    printf("Hola mundo\n");
-    system("pause");
+    int num1, num2, resultado;
+    printf("Primer valor:\n");
+    scanf("%d",&num1);
+    printf("Segundo valor:\n");
+    scanf("%d",&num2);
+    resultado = num1 + num2;
+    printf("Resultado:%d\n",resultado);
     return 0;
 
 
