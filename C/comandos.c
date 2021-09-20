34194e93fe870e83e5dd8851a7a00198ea8b7589
ManuManito77 <86896933+ManuManito77@users.noreply.github.com>
2021-09-20 17:39:31 -0300

    act

--- C/comandos.c
+++ C/comandos.c
@@ -1,4 +1,27 @@
 /*
+
+#include <>  ->  search in the default lib
+#include ""  ->  search the lib in the dir of the folder
+
+#define VAR(x) NUM  -> set the variable as NUM
+
+Variables:  
+%c char         1byte
+%i integer      2bytes
+&s short        2bytes
+&l long         4bytes
+%f float        4bytes
+%d double       8bytes
+
+ex:  %.3f  reduce the digits number
+
+unsigned  -> variable without sign
+
+int A ->  char A:  (char)A
+!!! An int as a char looks like a letter in ASCII
+EX:  (char)70 = P
+
+
 scanf("%d",&num1);
 
 printf("Resultado:%d\n",resultado);
@@ -15,5 +38,33 @@ if (---)
         ----------------
     }
 
+for (int i = 0; i < 5; i++)
+    {
+        printf("%i\n", i);
+    }
+
+while (contador <= 10)
+    {
+        printf("%i\n", contador);
+        contador++;
+    }
+
+do
+    {
+        printf(contador);
+        contador++;
+    } while (contador <= 10));
+
+switch (casos)
+    {
+    case 1:
+        printf("wena 1\n");
+        break;
+    case 2:
+        printf("wena 2\n");
+    default:
+        printf("no no no uwu\n");
+        break;
+    }
 
 */
--- C/test.c
+++ C/test.c
@@ -2,26 +2,8 @@
 #include <stdlib.h>
 
 int main(){
-    int x;
 
-    printf("Ingresa tu edad:\n");
-    scanf("%d",&x);
-
-    if (x >= 18)
-    {
-        printf("Wena\n");
-    }
-    else if (x == 17)
-    {
-        printf("kasjka casi pendejo");
-    }
-    else{
-        printf("no no no uwu");
-    }
-    
     
 
     return 0;
-
-
 }
